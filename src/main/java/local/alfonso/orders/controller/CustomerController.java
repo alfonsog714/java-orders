3645289420a8bea15d9a9212837131adfb6b1770
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-11 14:41:51 -0700

    Fleshed out the /customer/new endpoint for adding customers. Not exactly sure on how to test it though

--- src/main/java/local/alfonso/orders/controller/CustomerController.java
+++ src/main/java/local/alfonso/orders/controller/CustomerController.java
@@ -3,13 +3,14 @@ package local.alfonso.orders.controller;
 import local.alfonso.orders.model.Customers;
 import local.alfonso.orders.service.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.ArrayList;
 
 @RestController
@@ -30,5 +31,18 @@ public class CustomerController {
     {
         Customers customer = customersService.findCustomerByName(name);
         return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/new", consumes = {"application/json"}, produces = {"application/json"})
+    public ResponseEntity<?> addNewCustomer(@Valid @RequestBody Customers newCustomer)
+    {
+        newCustomer = customersService.save(newCustomer);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newCustomerURI = ServletUriComponentsBuilder.fromCurrentRequest().path("/{customerid}").buildAndExpand(newCustomer.getCustcode()).toUri();
+
+        responseHeaders.setLocation(newCustomerURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 }
