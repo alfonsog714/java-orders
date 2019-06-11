83ca35f400632b241b291ab0d971ef4f7be9086d
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-11 14:32:16 -0700

    C:/Program Files/Git/customer/name/{name} now working

--- src/main/java/local/alfonso/orders/controller/CustomerController.java
+++ src/main/java/local/alfonso/orders/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,12 @@ public class CustomerController {
     {
         ArrayList<Customers> rtnCus = customersService.findAll();
         return new ResponseEntity<>(rtnCus, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/name/{name}", produces = {"application/json"})
+    public ResponseEntity<?> getCustomerByName(@PathVariable String name)
+    {
+        Customers customer = customersService.findCustomerByName(name);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 }
